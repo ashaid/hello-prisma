e3106f972ae7c25adf3144e99795aff08c999168
tony <anthonyshaidaee@gmail.com>
2025-07-22 23:26:43 -0500

    create profile

--- express/src/index.ts
+++ express/src/index.ts
@@ -145,6 +145,24 @@ app.get("/feed", async (req, res) => {
   res.json(posts);
 });
 
+app.post("user/:id/profile", async (req, res) => {
+  const { id } = req.params;
+  const { bio } = req.body;
+
+  const profile = await prisma.profile.create({
+    data: {
+      bio,
+      user: {
+        connect: {
+          id: Number(id),
+        },
+      },
+    },
+  });
+
+  res.json(profile);
+});
+
 const server = app.listen(3000, () =>
   console.log(`
 🚀 Server ready at: http://localhost:3000
